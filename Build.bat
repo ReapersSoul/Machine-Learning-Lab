fe0809d224e0ed41396d3ff1ef0e7e43a44317b4
Trevor Ducharme <56406337+ReapersSoul@users.noreply.github.com>
2023-12-09 09:34:04 -0500

    win vs code config

--- Build.bat
+++ Build.bat
@@ -1,2 +1,4 @@
 cmake -B ./Build -S ./ -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake
-pause
+cmake --build ./Build --config Debug
+mkdir ./Build/Runtime/Debug/Machine-Learning-Lab/Libs
+Start ./Build/Runtime/Debug/Cleaner/Cleaner.exe
--- Cleaner/Cleaner.cpp
+++ Cleaner/Cleaner.cpp
@@ -6,7 +6,7 @@
 std::vector<std::string> LibsPath;
 std::vector<std::string> LibraryFiles;
 
-//returns the distance between two directories
+// returns the distance between two directories
 int DirectoryDistance(std::string path, std::string subpath)
 {
 	std::filesystem::path p1 = path;
@@ -14,7 +14,7 @@ int DirectoryDistance(std::string path, std::string subpath)
 
 	int distance = 0;
 
-	//use dijkstra's algorithm to find the distance between the two directories
+	// use dijkstra's algorithm to find the distance between the two directories
 	while (p1 != p2)
 	{
 		if (p1.string() == p2.string())
@@ -44,7 +44,7 @@ int DirectoryDistance(std::string path, std::string subpath)
 
 void FindLibs(std::string path, std::vector<std::string> Libraries, bool first)
 {
-	for (const auto& entry : std::filesystem::directory_iterator(path))
+	for (const auto &entry : std::filesystem::directory_iterator(path))
 	{
 		if (entry.is_directory())
 		{
@@ -70,79 +70,79 @@ void FindLibs(std::string path, std::vector<std::string> Libraries, bool first)
 	}
 }
 
-void FindLibs(std::string path,std::vector<std::string> Libraries)
+void FindLibs(std::string path, std::vector<std::string> Libraries)
 {
 	FindLibs(path, Libraries, true);
 }
 
 int main()
 {
-	//if the config file doesn't exist, create it
-	if (!std::filesystem::exists("config.json"))
+	try
 	{
-		//create the config file
-		std::ofstream o("config.json");
+		// if the config file doesn't exist, create it
+		if (!std::filesystem::exists("config.json"))
+		{
+			// create the config file
+			std::ofstream o("config.json");
 
-		//create the json object
+			// create the json object
+			nlohmann::json j;
+#if defined(_MSC_VER)
+			j["Libraries"] = {
+				"boost_filesystem-vc143-mt-gd-x64-1_83.dll",
+				"glew32d.dll",
+				"glfw3.dll",
+				"OpenCL.dll",
+				"sqlite3.dll",
+				"lua.dll"};
+#elif defined(__GNUC__)
+			j["Libraries"] = {
+				"libboost_filesystem-mgw82-mt-x64-1_73.so",
+				"libglew32d.so",
+				"libglfw3.so",
+				"libOpenCL.so",
+				"libsqlite3.so",
+				"liblua.so"};
+#endif
+
+			j["ExecutableDir"] = "../Machine-Learning-Lab";
+
+			// write the json object to the config file
+			o << j << std::endl;
+
+			// close the config file
+			o.close();
+		}
+
+		// load config.json
+		std::ifstream i("config.json");
 		nlohmann::json j;
-		#if defined(_MSC_VER)
-			j["Libraries"] = {
-					"boost_filesystem-vc143-mt-gd-x64-1_83.dll",
-					"glew32d.dll",
-					"glfw3.dll",
-					"OpenCL.dll",
-					"sqlite3.dll",
-					"lua.dll"
-			};
-		#elif defined(__GNUC__)
-			j["Libraries"] = {
-					"libboost_filesystem-mgw82-mt-x64-1_73.so",
-					"libglew32d.so",
-					"libglfw3.so",
-					"libOpenCL.so",
-					"libsqlite3.so",
-					"liblua.so"
-			};
-		#endif
+		i >> j;
 
-		j["ExecutableDir"] = "../Machine-Learning-Lab";
+		std::vector<std::string> Libraries = j["Libraries"];
 
-		//write the json object to the config file
-		o << j << std::endl;
+		FindLibs(j["ExecutableDir"], Libraries);
 
-		//close the config file
-		o.close();
+		for (int i = 0; i < LibraryFiles.size(); i++)
+		{
+			std::vector<std::pair<int, std::string>> distances;
+			for (int j = 0; j < LibsPath.size(); j++)
+			{
+				distances.push_back(std::make_pair(DirectoryDistance(LibsPath[j], LibraryFiles[i]), LibsPath[j]));
+			}
+			std::sort(distances.begin(), distances.end(), [](std::pair<int, std::string> a, std::pair<int, std::string> b)
+					  {  return a.first < b.first; });
+			printf("Found library %s Closest directory is %s\n", LibraryFiles[i].c_str(), distances[0].second.c_str());
+			// move the file
+
+			// if the file already exists, delete it
+			std::filesystem::copy(LibraryFiles[i], distances[0].second, std::filesystem::copy_options::overwrite_existing);
+			// delete the file
+			std::filesystem::remove(LibraryFiles[i]);
+		}
 	}
-
-	//load config.json
-	std::ifstream i("config.json");
-	nlohmann::json j;
-	i >> j;
-
-	
-	std::vector<std::string> Libraries = j["Libraries"];
-
-	FindLibs(j["ExecutableDir"], Libraries);
-
-	for (int i = 0; i < LibraryFiles.size(); i++)
+	catch (std::exception e)
 	{
-		std::vector<std::pair<int, std::string>> distances;
-		for (int j = 0; j < LibsPath.size(); j++)
-		{
-			distances.push_back(std::make_pair(DirectoryDistance(LibsPath[j], LibraryFiles[i]), LibsPath[j]));
-		}
-		std::sort(distances.begin(), distances.end(), [](std::pair<int, std::string> a, std::pair<int, std::string> b) {return a.first < b.first; });
-		printf("Found library %s Closest directory is %s\n", LibraryFiles[i].c_str(), distances[0].second.c_str());
-		//move the file
-		try{
-		//if the file already exists, delete it
-		std::filesystem::copy(LibraryFiles[i], distances[0].second, std::filesystem::copy_options::overwrite_existing);
-		//delete the file
-		std::filesystem::remove(LibraryFiles[i]);
-		}
-		catch (std::exception e)
-		{
-			printf("Error moving file %s: %s\n", LibraryFiles[i].c_str(), e.what());
-		}
+		printf("Error: %s\n", e.what());
 	}
 }
