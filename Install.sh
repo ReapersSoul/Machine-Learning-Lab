776d5c6d468040196b34ab2f7968e443bf4f1588
ReapersSoul <18trevor21@gmail.com>
2023-12-18 17:20:50 -0500

    Instal change

--- Install.sh
+++ Install.sh
@@ -1,28 +1,28 @@
-sudo apt-get install gcc build-essential gdb cmake curl zip unzip tar pkg-config autoconf autoconf-archive libxmu-dev libxi-dev libgl-dev libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev
+apt-get install gcc build-essential gdb cmake curl zip unzip tar pkg-config autoconf autoconf-archive libxmu-dev libxi-dev libgl-dev libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev
 
-#git clone https://github.com/Microsoft/vcpkg.git /vcpkg
-#chmod +x /vcpkg/bootstrap-vcpkg.sh
+#sudo git clone https://github.com/Microsoft/vcpkg.git /vcpkg
+#sudo chmod +x /vcpkg/bootstrap-vcpkg.sh
 #export VCPKG_FORCE_SYSTEM_BINARIES=1
 #export PATH=/vcpkg:$PATH
+#sudo ./vcpkg/bootstrap-vcpkg.sh
 
-sudo bootstrap-vcpkg.sh
-sudo vcpkg install nlohmann-json:x64-linux --recurse
-sudo vcpkg install sqlite3:x64-linux --recurse
-sudo vcpkg install opencl:x64-linux --recurse
-sudo vcpkg install opengl:x64-linux --recurse
-sudo vcpkg install glew:x64-linux --recurse
-sudo vcpkg install glfw3:x64-linux --recurse
-sudo vcpkg install glm:x64-linux --recurse
-sudo vcpkg install imgui[docking-experimental,opengl3-binding,glfw-binding]:x64-linux --recurse
-sudo vcpkg install imgui-node-editor:x64-linux --recurse
-sudo vcpkg install lua:x64-linux --recurse
-sudo vcpkg install chaiscript:x64-linux --recurse
-sudo vcpkg install duktape:x64-linux --recurse
-sudo vcpkg install python3:x64-linux --recurse
-sudo vcpkg install stb:x64-linux --recurse
-sudo vcpkg install boost:x64-linux --recurse
-sudo vcpkg install boost-dll:x64-linux --recurse
-sudo vcpkg integrate install
+vcpkg install nlohmann-json:x64-linux --recurse
+vcpkg install sqlite3:x64-linux --recurse
+vcpkg install opencl:x64-linux --recurse
+vcpkg install opengl:x64-linux --recurse
+vcpkg install glew:x64-linux --recurse
+vcpkg install glfw3:x64-linux --recurse
+vcpkg install glm:x64-linux --recurse
+vcpkg install imgui[docking-experimental,opengl3-binding,glfw-binding]:x64-linux --recurse
+vcpkg install imgui-node-editor:x64-linux --recurse
+vcpkg install lua:x64-linux --recurse
+vcpkg install chaiscript:x64-linux --recurse
+vcpkg install duktape:x64-linux --recurse
+vcpkg install python3:x64-linux --recurse
+vcpkg install stb:x64-linux --recurse
+vcpkg install boost:x64-linux --recurse
+vcpkg install boost-dll:x64-linux --recurse
+vcpkg integrate install
 
-#chmod +x ./Build.sh
+#sudo chmod +x ./Build.sh
 #sudo ./Build.sh
