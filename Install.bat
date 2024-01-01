02d927e8ed9ab0dea374892af2f14b7544bb5a75
Bacon <iam@baconing.tech>
2023-12-31 19:52:12 -0500

    build(vcpkg): use `vcpkg.json` for dependencies rather than installing seperately in a script

--- Install.bat
+++ Install.bat
@@ -1,20 +1,5 @@
 git clone https://github.com/Microsoft/vcpkg.git
-sudo ./vcpkg/bootstrap-vcpkg.bat
-sudo ./vcpkg/vcpkg install nlohmann-json:x64-windows --recurse
-sudo ./vcpkg/vcpkg install sqlite3:x64-windows --recurse
-sudo ./vcpkg/vcpkg install opencl:x64-windows --recurse
-sudo ./vcpkg/vcpkg install opengl:x64-windows --recurse
-sudo ./vcpkg/vcpkg install glew:x64-windows --recurse
-sudo ./vcpkg/vcpkg install glfw3:x64-windows --recurse
-sudo ./vcpkg/vcpkg install glm:x64-windows --recurse
-sudo ./vcpkg/vcpkg install imgui[docking-experimental,opengl3-binding,glfw-binding]:x64-windows --recurse
-sudo ./vcpkg/vcpkg install imgui-node-editor:x64-windows --recurse
-sudo ./vcpkg/vcpkg install lua:x64-windows --recurse --recurse
-sudo ./vcpkg/vcpkg install chaiscript:x64-windows --recurse
-sudo ./vcpkg/vcpkg install duktape:x64-windows --recurse
-sudo ./vcpkg/vcpkg install python3:x64-windows --recurse
-sudo ./vcpkg/vcpkg install stb:x64-windows --recurse
-sudo ./vcpkg/vcpkg install boost:x64-windows --recurse
-sudo ./vcpkg/vcpkg install boost-dll:x64-windows --recurse
-sudo ./vcpkg/vcpkg integrate install
-./Build.bat
+.\vcpkg\bootstrap-vcpkg.bat
+
+.\vcpkg\vcpkg install
+.\vcpkg\vcpkg integrate install
--- Install.sh
+++ Install.sh
@@ -6,22 +6,7 @@ apt-get install gcc build-essential gdb cmake curl zip unzip tar pkg-config auto
 #export PATH=/vcpkg:$PATH
 #sudo ./vcpkg/bootstrap-vcpkg.sh
 
-vcpkg install nlohmann-json:x64-linux --recurse
-vcpkg install sqlite3:x64-linux --recurse
-vcpkg install opencl:x64-linux --recurse
-vcpkg install opengl:x64-linux --recurse
-vcpkg install glew:x64-linux --recurse
-vcpkg install glfw3:x64-linux --recurse
-vcpkg install glm:x64-linux --recurse
-vcpkg install imgui[docking-experimental,opengl3-binding,glfw-binding]:x64-linux --recurse
-vcpkg install imgui-node-editor:x64-linux --recurse
-vcpkg install lua:x64-linux --recurse
-vcpkg install chaiscript:x64-linux --recurse
-vcpkg install duktape:x64-linux --recurse
-vcpkg install python3:x64-linux --recurse
-vcpkg install stb:x64-linux --recurse
-vcpkg install boost:x64-linux --recurse
-vcpkg install boost-dll:x64-linux --recurse
+vcpkg install
 vcpkg integrate install
 
 #sudo chmod +x ./Build.sh
